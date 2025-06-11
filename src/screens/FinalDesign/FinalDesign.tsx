3c9cda5847da7fe874478fa6ba6a0be83e3f9539
Ago Chukwubuikem <agochukwubuikejideofor@gmail.com>
2025-06-11 22:22:26 +0100

    fixing the width issue

--- src/screens/FinalDesign/FinalDesign.tsx
+++ src/screens/FinalDesign/FinalDesign.tsx
@@ -8,7 +8,7 @@ import { ProjectsSection } from "./sections/ProjectsSection/ProjectsSection";
 export const FinalDesign = (): JSX.Element => {
   return (
     <main className="bg-white w-full">
-      <div className="mx-auto max-w-[1440px]">
+      <div className="mx-auto max-full-[1440px]">
         <HeroSection />
         <AboutMeSection />
         <ProjectsSection />
